0664c5a610ee818973beb7e8c98fcb37236f1442
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-02-19 18:53:43 -0300

    refact: add nav bar into shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { AddProfileFormsComponent } from './views/add-profile-forms/add-profile-forms.component';
+import { CreateProfileFormsComponent } from './views/create-profile-forms/create-profile-forms.component';
 
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, NavBarComponent],
+  declarations: [AppComponent, SideBarComponent, NavBarComponent, AddProfileFormsComponent, CreateProfileFormsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
