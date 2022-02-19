292f65b85e5e6748bf6e03a4ada0a56d3fea311a
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-02-19 20:17:15 -0300

    feat: add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateProfileFormsComponent } from './views/create-profile-forms/create-profile-forms.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'create-profile',
+    component: CreateProfileFormsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
