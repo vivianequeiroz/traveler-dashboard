19cc52d70bd472139a8075ce5da87f4417f7b1a2
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-02-23 19:22:53 -0300

    update: shared module components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HeaderFormsComponent } from './components/header-forms/header-forms.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { CityBaicDataFormsComponent } from './components/city-baic-data-forms/city-baic-data-forms.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SideBarComponent, NavBarComponent, HeaderFormsComponent],
+  declarations: [SideBarComponent, NavBarComponent, HeaderFormsComponent, CityBaicDataFormsComponent],
   exports: [SideBarComponent, NavBarComponent, HeaderFormsComponent],
 })
 export class SharedModule {}
