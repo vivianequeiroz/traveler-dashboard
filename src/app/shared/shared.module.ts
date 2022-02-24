d555ff99f5c588366f4332f7612bce7e23b9bb66
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-02-24 20:32:48 -0300

    chore: update shared module components and module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { CityBasicDataFormsComponent } from './components/city-basic-data-forms/city-basic-data-forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { CityDataService } from '../services/city/city-data.service';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(CityDataService),
+  ],
   declarations: [
     SideBarComponent,
     NavBarComponent,
