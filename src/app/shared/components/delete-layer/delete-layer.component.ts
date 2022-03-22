09c79d9f6a70b47b89b11bdcdc2c09837bae69c1
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-03-21 23:03:56 -0300

    chore: delete layer component

---
+++ src/app/shared/components/delete-layer/delete-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-layer',
+  templateUrl: './delete-layer.component.html',
+  styleUrls: ['./delete-layer.component.scss']
+})
+export class DeleteLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
